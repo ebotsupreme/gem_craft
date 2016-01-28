71b204186e41a86c55d8806ad3069b8a81302712
Eddie​ <ejung716@gmail.com>
2016-01-27 19:06:17 -0800

    added bootstrap to the main page and added images for each gem; also added ng-src to display the first full gem img

--- app.js
+++ app.js
@@ -8,16 +8,29 @@
   var gems = [
     {
       name: 'Dodecahedron',
-      price: 2.95,
+      price: 2,
       description: 'Very shiny.',
       canPurchase: true,
-      soldOut: true
+      soldOut: true,
+      images:[
+        {
+          full: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Pentakisdodecahedron.jpg/280px-Pentakisdodecahedron.jpg',
+          thumb: 'http://farm8.static.flickr.com/7503/15729507095_a90d3b495c_t.jpg'
+        }
+      ]
     },
     {
       name: 'Pentagonal Gem',
+      price: 5.95,
       description: 'Origin unknown.',
       canPurchase: true,
-      soldOut: false
+      soldOut: false,
+      images:[
+        {
+          full: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQPMmKKOEkhyRWU60w_sn3ZwzM7qtTSrCPQoEEW0F7bKnUeRiBx',
+          thumb: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTRxxZdxwGbnHFRqeUHaMqrkVwdKP7qNxo6BWypyJiWtErvNGTW5w'
+        }
+      ]
     }
   ];
 })();
